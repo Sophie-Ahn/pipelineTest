78c1b54a8365dcea56a81b2751115f77f45817b8
Sophie-Ahn <sgdream95@gmail.com>
2024-06-05 10:31:56 +0900

    first commit

---
+++ pipeline/src/test/java/test/jenkins/pipeline/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package test.jenkins.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
